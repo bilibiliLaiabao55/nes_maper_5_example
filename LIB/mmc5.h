c6aaaf6d19aac3202449cba0d50b793e4791117b
bilibiliLaiabao55 <bilibili_laiabao@126.com>
2025-05-01 19:40:22 +0800

    Add files via upload

---
+++ LIB/mmc5.h
@@ -0,0 +1,8 @@
+void __fastcall__ set_prg_mode(unsigned char mode);
+void __fastcall__ set_chr_mode(unsigned char mode);
+
+void __fastcall__ set_prg_5113(unsigned char bankID);
+void __fastcall__ set_prg_5114(unsigned char bankID);
+void __fastcall__ set_prg_5115(unsigned char bankID);
+void __fastcall__ set_prg_5116(unsigned char bankID);
+void __fastcall__ set_prg_5117(unsigned char bankID);
---
+++ LIB/mmc5.s
@@ -0,0 +1,24 @@
+; [A] mode
+_set_prg_mode:
+    sta $5100
+    rts
+_set_chr_mode:
+    sta $5101
+    rts
+
+; [A] bankID
+_set_prg_5113:
+    sta $5113
+    rts
+_set_prg_5114:
+    sta $5114
+    rts
+_set_prg_5115:
+    sta $5115
+    rts
+_set_prg_5116:
+    sta $5116
+    rts
+_set_prg_5117:
+    sta $5117
+    rts
